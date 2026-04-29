1202c8e88168328c7eb3cd42377f5db73bf5b3d5
rahulkannan08 <rahul08supermacy@gmail.com>
2026-04-29 22:10:09 +0530

    1,2

---
+++ pg2.r
@@ -0,0 +1,22 @@
+thisarray<-c(1:24)
+thisarray
+multiarray<-array(thisarray, dim =c(4,3,2))
+multiarray
+thisarray<-c(1:24)
+multiarray<-array(thisarray, dim=c(4,3,2))
+multiarray[2,3,2]
+thisarray<-c(1:24)
+multiarray<-array(thisarray, dim=c(4,3,2))
+2%in%multiarray
+thisarray<-c(1:24)
+multiarray<-array(thisarray, dim=c(4,3,2))
+dim(multiarray)
+thisarray<-c(1:24)
+multiarray<-array(thisarray ,dim=c(4,3,2))
+length(multiarray)
+thisarray<-c(1:24)
+multiarray<-array(thisarray, dim=c(4,3,2))
+for(x in multiarray){
+  print(x)
+}
+
